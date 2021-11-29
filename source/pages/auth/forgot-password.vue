2501bd9a49d08424ebc762165a91a3d951be23e1
Do Thi Huong <huong.do@kiaisoft.com>
2021-11-29 16:56:57 +0700

    validate and reset password

--- source/pages/auth/forgot-password.vue
+++ source/pages/auth/forgot-password.vue
@@ -10,24 +10,24 @@
           <div v-if="message" class="alert alert-success" role="alert">
             {{message}}
           </div>
+          <div v-if="error" class="alert alert-danger" role="alert">
+            {{error}}
+          </div>
 
           <div class="form-group my-5">
             <label for="email">メールアドレス</label>
             <div class="input-group has-validation">
               <span class="input-group-text input-group-text-pre"><img src="../../assets/images/icon_email.svg" alt=""></span>
               <input id="email"
-                     v-model="email"
-                     type="email"
+                     v-model="$v.email.$model"
                      class="form-control form-control-lg"
                      aria-describedby="emailHelp"
-                     required
-                     oninvalid="this.setCustomValidity('Please fill out your email')"
-                     oninput="setCustomValidity('')"
                      maxlength="50"
               >
             </div>
-            <div v-if="errors">
-              <p v-for="(error, key) in (errors.email)" :key="key" class="text-danger">{{error}}</p>
+            <div v-if="$v.email.$error">
+              <div v-if="!$v.email.required" class="error">これは必須項目なので、必ず入力してください</div>
+              <div v-if="!$v.email.email" class="error">メールアドレスの形式で入力してください</div>
             </div>
           </div>
           <button type="submit" class="btn fw-bold">送信</button>
@@ -43,15 +43,25 @@
 
 <script>
   import 'bootstrap/dist/css/bootstrap.css';
+  import {validationMixin} from 'vuelidate'
+  import {required, email} from 'vuelidate/lib/validators'
 
   export default {
     name: "ForgotPassword",
+    mixins: [validationMixin],
 
     data() {
       return {
         email: '',
         message: '',
-        errors: []
+        error: ''
+      }
+    },
+
+    validations: {
+      email: {
+        required,
+        email,
       }
     },
 
@@ -61,17 +71,21 @@
 
     methods: {
       async forgotPassword() {
-        try {
-          await this.$repositories.account.forgotPassword({
-            email: this.email
-          })
-            .then((res) => {
-              const data = this.$handleResponse(res);
-              this.message = data.message;
-              this.errors = data.errors;
-          });
-        } catch (e) {
-          console.log(e.message)
+        this.$v.$touch();
+        if (!this.$v.$invalid) {
+          try {
+            await this.$repositories.account.forgotPassword({
+              email: this.email
+            })
+              .then((res) => {
+                const data = this.$handleResponse(res);
+                this.message = data.message;
+                this.error = data.errorMsg;
+              });
+          } catch (e) {
+            this.message = '';
+            this.error = e.response.data.message;
+          }
         }
       },
     }
