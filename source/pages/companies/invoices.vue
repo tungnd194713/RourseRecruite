8917e0c9fd857f476376fc27f885d70c2e31bf3f
Do Thi Huong <huong.do@kiaisoft.com>
2021-12-17 15:18:56 +0700

    stash

--- source/pages/companies/invoices.vue
+++ source/pages/companies/invoices.vue
@@ -18,7 +18,7 @@
               <td class="align-middle">{{ item.id }}</td>
               <td v-if="item.paid_at" class="align-middle py-3">{{ item.paid_at.split(' ')[0] }}</td>
               <td v-else class="align-middle py-3"></td>
-              <td class="align-middle" @click="$router.push('/invoices/detail?year_month=' + item.year_month)">{{ item.year_month }}</td>
+              <td class="align-middle" @click="$router.push('/invoices/detail?id=' + item.id)">{{ item.year_month }}</td>
               <td class="align-middle">
                 ¥{{ item.cost_job ? Math.ceil(item.cost_job).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : null }}
               </td>
@@ -38,12 +38,28 @@
                 ¥{{ item.total ? Math.ceil(item.total).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : null }}
               </td>
               <td class="align-middle">
-                <div class="btn-payment">
+                <div v-if="item.status === 2" class="btn-payment">
                   <img
-                    class="text-center btn-inside"
+                    class="text-center btn btn-inside"
                     src="../../assets/images/icon_money_jp.svg"
                     alt=""
+                    data-bs-toggle="modal"
+                    data-bs-target="#checkoutPayjpModal"
                   />
+                  <form @submit.prevent="submitPayment">
+                    <script
+                      type="text/javascript"
+                      src="https://checkout.pay.jp/"
+                      class="payjp-button"
+                      :data-key="pk_key"
+                      data-submit-text="トークンを作成する"
+                      data-partial="false"
+                      data-token-name="payjp_token"
+                      :data-on-created="submitPayment"
+                      :data-on-failed="onTokenFailed"
+                    >
+                    </script>
+                  </form>
                 </div>
               </td>
             </tr>
@@ -60,11 +76,103 @@
         @customPage="pageChangeHandle"
       />
     </div>
+    <!-- Modal -->
+    <div id="checkoutPayjpModal" class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 id="exampleModalLabel" class="modal-title">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="card_block__body">
+              <div id="payjp_spinner" class="spinner" style="display: none;"><img src="images/loading.gif"></div>
+              <div class="tab-content">
+                <div id="payjp_supportedBrands" class="mod--cardlist">
+                  <ul>
+                    <li><img src="https://checkout.pay.jp/images/creditcard/visa.png"></li>
+                    <li><img src="https://checkout.pay.jp/images/creditcard/mastercard.png"></li>
+                    <li><img src="https://checkout.pay.jp/images/creditcard/jcb.png"></li>
+                    <li><img src="https://checkout.pay.jp/images/creditcard/americanExpress.png"></li>
+                    <li><img src="https://checkout.pay.jp/images/creditcard/discover.png"></li>
+                    <li><img src="https://checkout.pay.jp/images/creditcard/dinersClub.png"></li>
+                  </ul>
+                </div>
+                <div id="account_card" role="tabpanel" class="tab-pane active">
+                  <form>
+                    <div class="mod--form">
+                      <div class="form_control">
+                        <div class="inner"><label for="payjp_cardNumber">カード</label><input id="payjp_cardNumber"
+                                                                                           name="cardnumber"
+                                                                                           class="payjp_simple-input-text"
+                                                                                           type="tel"
+                                                                                           placeholder="1234 5678 9012 3456"
+                                                                                           maxlength="19"
+                                                                                           pattern="([0-9]| )*">
+                          <hr class="payjp_border">
+                          <hr class="payjp_hiddenborder">
+                          <div id="payjp_cardImage" class="card"></div>
+                        </div>
+                      </div>
+                      <div class="form_control form_control--half">
+                        <div class="inner"><label for="payjp_cardExpiresMonth">有効期限</label> <input
+                          id="payjp_cardExpiresMonth" name="ccmonth" class="payjp_simple-input-text" autocomplete="off"
+                          inputtype="number" placeholder="月" type="tel" maxlength="2"> <input id="payjp_cardExpiresYear"
+                                                                                              name="ccyear"
+                                                                                              class="payjp_simple-input-text"
+                                                                                              autocomplete="off"
+                                                                                              inputtype="number"
+                                                                                              placeholder="年" type="tel"
+                                                                                              maxlength="2"> <input
+                          id="payjp_cardExpiresSeparator" class="payjp_simple-input-separator" placeholder="/"
+                          type="text" disabled="">
+                          <hr class="payjp_border">
+                          <hr class="payjp_hiddenborder">
+                        </div>
+                        <span id="payjp_partition-border"></span>
+                        <div class="inner"><label for="payjp_cardCvc">CVC番号</label> <input id="payjp_cardCvc" name="cvc"
+                                                                                           class="payjp_simple-input-text"
+                                                                                           autocomplete="off" type="tel"
+                                                                                           maxlength="4"
+                                                                                           placeholder="CVC">
+                          <hr class="payjp_border">
+                          <hr class="payjp_hiddenborder">
+                          <div id="payjp_cvcTip" class="help_tip" style="display: none;"><img
+                            src="images/cvc_tip/type_01.png"></div>
+                          <div id="payjp_cvcTipMobile" class="help_tip__mobile" style="display: none;"><img
+                            src="images/cvc_tip/type_01_m.png"></div>
+                          <button id="payjp_cvcTipButton" class="help_tip_button" type="button" tabindex="-1"></button>
+                        </div>
+                      </div>
+                      <div class="form_control">
+                        <div class="inner"><label for="payjp_cardName">名前</label><input id="payjp_cardName"
+                                                                                        name="ccname"
+                                                                                        class="payjp_simple-input-text"
+                                                                                        type="text" inputtype="email"
+                                                                                        placeholder="TARO YAMADA">
+                          <hr class="payjp_border">
+                          <hr class="payjp_hiddenborder">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="bottom_block"><input id="payjp_cardSubmit" type="submit" value="トークンを作成する" class="btn">
+                    </div>
+                  </form>
+                </div></div></div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css'
+// import _ from 'lodash'
 import Pagination from '../../components/Pagination'
 import theStatus from '~/constants/invoicesStatus'
 
@@ -120,6 +228,7 @@ export default {
       perPage: 10,
       totalItems: 0,
       pageCount: 1,
+      pk_key: process.env.PAYJP_PUBLIC_KEY
     }
   },
 
@@ -140,6 +249,7 @@ export default {
 
   created() {
     this.getInvoices(this.currentPage)
+    // this.submitPayment()
   },
 
   methods: {
@@ -169,7 +279,28 @@ export default {
       }
       this.getInvoices(this.currentPage)
     },
-  },
+    async submitPayment(res) {
+      // eslint-disable-next-line no-use-before-define
+      // const token = this.$route.query.payjp_token ?? null;
+      console.log(123)
+      // if (token) {
+        const dataform = new FormData();
+
+        // dataform.append('id', this.$root.user.id);
+        dataform.append('payjp-token', res.id);
+
+        await this.$axios.post('/companies/payment', dataform).then(e => {
+          console.log(e);
+        }).catch((error) => {
+          console.log(error);
+        });
+      // }
+    },
+    onTokenFailed (status, err) {
+      console.log(status)
+      console.log(err)
+    }
+  }
 }
 </script>
 
